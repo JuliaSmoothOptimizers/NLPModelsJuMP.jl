05d4d9b980c3da91acda1c453a2f3c22b13fbced
Abel Soares Siqueira <abel.s.siqueira@gmail.com>
2018-08-17 10:13:30 -0300

    First commit
    Package created from NLPModels.jl

---
+++ src/NLPJuMPModels.jl
@@ -0,0 +1,9 @@
+#__precompile__()
+
+module NLPJuMPModels
+
+include("nlsmpb_model.jl")
+include("nlp_to_mpb.jl")
+include("jump_model.jl")
+
+end
---
+++ test/runtests.jl
@@ -0,0 +1,9 @@
+using Base.Test, JuMP, NLPJuMPModels
+
+for problem in [:brownden, :hs5, :hs6, :hs10, :hs11, :hs14, :hs30, :hs43, :mgh07, :mgh35]
+  include("$problem.jl")
+end
+
+include("test_mpb.jl")
+include("consistency.jl")
+include("test_mathprognlsmodel.jl")
