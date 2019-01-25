b1c7ebf41145f05b03e77ba4e95fc9069d5783df
Abel Soares Siqueira <abel.s.siqueira@gmail.com>
2019-01-24 21:21:24 -0500

    Update nls_consistency

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,20 +1,17 @@
 using Test, JuMP, NLPModels, NLPModelsJuMP, LinearAlgebra,
       SparseArrays, Printf
 
-@static if VERSION < v"0.7"
-  path = joinpath(Pkg.dir("NLPModels"), "test")
-else
-  path = joinpath(dirname(pathof(NLPModels)), "..", "test")
-end
+nlpmodels_path = joinpath(dirname(pathof(NLPModels)), "..", "test")
 
 for problem in [:brownden, :hs5, :hs6, :hs10, :hs11, :hs14, :hs30, :hs43, :mgh07, :mgh35]
   include("$problem.jl")
-  if isfile(joinpath(path, "$problem.jl"))
-    include(joinpath(path, "$problem.jl"))
+  if isfile(joinpath(nlpmodels_path, "$problem.jl"))
+    include(joinpath(nlpmodels_path, "$problem.jl"))
   end
 end
 
 include("test_mpb.jl")
 include("consistency.jl")
+include("nls_consistency.jl")
 include("test_mathprognlsmodel.jl")
 include("test_view_subarray.jl")
