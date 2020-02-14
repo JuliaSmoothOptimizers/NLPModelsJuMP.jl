325f3c1176e7c0f0558a23fad0d89606d1a29d05
Alexis Montoison <alexis.montoison@polymtl.ca>
2020-02-13 20:35:02 -0500

    Use the correct consistency path

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,12 +3,11 @@ using LinearAlgebra, SparseArrays
 using Test, Printf
 
 nlpmodels_path = joinpath(dirname(pathof(NLPModels)), "..", "test")
-nlpmodels_problems_path = joinpath(nlpmodels_path, "problems")
 
 for problem in [:brownden, :hs5, :hs6, :hs10, :hs11, :hs14]
   include(joinpath("problems", "$problem.jl"))
-  if isfile(joinpath(nlpmodels_problems_path, "$problem.jl"))
-    include(joinpath(nlpmodels_problems_path, "$problem.jl"))
+  if isfile(joinpath(nlpmodels_path, "$problem.jl"))
+    include(joinpath(nlpmodels_path, "$problem.jl"))
   end
 end
 
