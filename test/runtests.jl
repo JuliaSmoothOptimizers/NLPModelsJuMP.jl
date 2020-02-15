726642dae25b81b03c397d72221c0ab2bf6b2f43
Dominique Orban <dominique.orban@gmail.com>
2020-02-15 15:01:33 -0500

    remove tests with IPOPT in preparation for #37

--- test/runtests.jl
+++ test/runtests.jl
@@ -10,7 +10,6 @@ for problem in [:brownden, :hs5, :hs6, :hs10, :hs11, :hs14, :hs30, :hs43, :mgh07
   end
 end
 
-include("test_mpb.jl")
 include("consistency.jl")
 include("nls_consistency.jl")
 include("test_mathprognlsmodel.jl")
