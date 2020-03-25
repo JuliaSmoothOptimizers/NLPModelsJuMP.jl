bd1818665a828f0da6bd826b77da81d2bb6ee6da
Alexis Montoison <alexis.montoison@polymtl.ca>
2020-03-24 22:13:55 -0400

    Add parser functions

--- src/utils.jl
+++ src/utils.jl
@@ -1,3 +1,25 @@
+using NLPModels
+import NLPModels.increment!, NLPModels.decrement!
+
+using JuMP, MathOptInterface
+const MOI = MathOptInterface
+
+# ScalarAffineFunctions and VectorAffineFunctions
+const SAF = MOI.ScalarAffineFunction{Float64}
+const VAF = MOI.VectorAffineFunction{Float64}
+const AF  = Union{SAF, VAF}
+
+# AffLinSets and VecLinSets
+const ALS = Union{MOI.EqualTo{Float64}, MOI.GreaterThan{Float64}, MOI.LessThan{Float64}, MOI.Interval{Float64}}
+const VLS = Union{MOI.Nonnegatives, MOI.Nonpositives, MOI.Zeros}
+const LS  = Union{ALS, VLS}
+
+mutable struct LinearConstraints
+  rows :: Vector{Int}
+  cols :: Vector{Int}
+  vals :: Vector{Float64}
+end
+
 """
     replace!(ex, x)
 
@@ -15,4 +37,133 @@ function replace!(ex, x)
       end
     end
   end
+end
+
+"""
+    parser_SAF(fun, set, linrows, lincols, linvals, nlin, lin_lcon, lin_ucon)
+
+Parse a `ScalarAffineFunction` fun with its associated set.
+`linrows`, `lincols`, `linvals`, `lin_lcon` and `lin_ucon` are updated.
+"""
+function parser_SAF(fun, set, linrows, lincols, linvals, nlin, lin_lcon, lin_ucon)
+
+  # Parse a ScalarAffineTerm{Float64}(coefficient, variable_index)
+  for term in fun.terms
+    push!(linrows, nlin + 1)
+    push!(lincols, term.variable_index.value)
+    push!(linvals, term.coefficient)
+  end
+
+  if typeof(set) in (MOI.Interval{Float64}, MOI.GreaterThan{Float64})
+    push!(lin_lcon, -fun.constant + set.lower)
+  elseif typeof(set) == MOI.EqualTo{Float64}
+    push!(lin_lcon, -fun.constant + set.value)
+  else
+    push!(lin_lcon, -Inf)
+  end
+
+  if typeof(set) in (MOI.Interval{Float64}, MOI.LessThan{Float64})
+    push!(lin_ucon, -fun.constant + set.upper)
+  elseif typeof(set) == MOI.EqualTo{Float64}
+    push!(lin_ucon, -fun.constant + set.value)
+  else
+    push!(lin_ucon, Inf)
+  end
+end
+
+"""
+    parser_VAF(fun, set, linrows, lincols, linvals, nlin, lin_lcon, lin_ucon)
+
+Parse a `VectorAffineFunction` fun with its associated set.
+`linrows`, `lincols`, `linvals`, `lin_lcon` and `lin_ucon` are updated.
+"""
+function parser_VAF(fun, set, linrows, lincols, linvals, nlin, lin_lcon, lin_ucon)
+
+  # Parse a VectorAffineTerm{Float64}(output_index, scalar_term)
+  for term in fun.terms
+    push!(linrows, nlin + term.output_index)
+    push!(lincols, term.scalar_term.variable_index.value)
+    push!(linvals, term.scalar_term.coefficient)
+  end
+
+  if typeof(set) in (MOI.Nonnegatives, MOI.Zeros)
+    append!(lin_lcon, -fun.constants)
+  else
+    append!(lin_lcon, -Inf * ones(set.dimension))
+  end
+
+  if typeof(set) in (MOI.Nonpositives, MOI.Zeros)
+    append!(lin_ucon, -fun.constants)
+  else
+    append!(lin_ucon, Inf * ones(set.dimension))
+  end
+end
+
+"""
+    parser_MOI(moimodel)
+
+Parse linear constraints of a `MOI.ModelLike`.
+"""
+function parser_MOI(moimodel)
+
+  # Variables associated to linear constraints
+  nlin     = 0
+  linrows  = Int[]
+  lincols  = Int[]
+  linvals  = Float64[]
+  lin_lcon = Float64[]
+  lin_ucon = Float64[]
+
+  contypes = MOI.get(moimodel, MOI.ListOfConstraints())
+  for (F, S) in contypes
+    F == MOI.SingleVariable && continue
+    F <: AF || warning("Function $F is not supported.")
+    S <: LS || warning("Set $S is not supported.")
+
+    conindices = MOI.get(moimodel, MOI.ListOfConstraintIndices{F, S}())
+    for cidx in conindices
+      fun = MOI.get(moimodel, MOI.ConstraintFunction(), cidx)
+      set = MOI.get(moimodel, MOI.ConstraintSet(), cidx)
+      if typeof(fun) <: SAF
+        parser_SAF(fun, set, linrows, lincols, linvals, nlin, lin_lcon, lin_ucon)
+        nlin += 1
+      end
+      if typeof(fun) <: VAF
+        parser_VAF(fun, set, linrows, lincols, linvals, nlin, lin_lcon, lin_ucon)
+        nlin += set.dimension
+      end
+    end
+  end
+
+  return nlin, linrows, lincols, linvals, lin_lcon, lin_ucon
+end
+
+"""
+    parser_JuMP(jmodel)
+
+Parse variables informations and non-linear constaints of a `JuMP.Model`.
+"""
+function parser_JuMP(jmodel)
+
+  # Number of variables and bounds constraints
+  nvar = Int(num_variables(jmodel))
+  vars = all_variables(jmodel)
+  lvar = map(var -> has_lower_bound(var) ? lower_bound(var) : -Inf, vars)
+  uvar = map(var -> has_upper_bound(var) ? upper_bound(var) :  Inf, vars)
+
+  # Initial solution
+  x0 = zeros(nvar)
+  for (i, val) ∈ enumerate(start_value.(vars))
+    if val !== nothing
+      x0[i] = val
+    end
+  end
+
+  # Variables associated to non-linear constraints
+  nnln = num_nl_constraints(jmodel)
+  nl_cons = jmodel.nlp_data.nlconstr
+  nl_lcon = map(nl_con -> nl_con.lb, nl_cons)
+  nl_ucon = map(nl_con -> nl_con.ub, nl_cons)
+
+  return nvar, lvar, uvar, x0, nnln, nl_lcon, nl_ucon
 end
