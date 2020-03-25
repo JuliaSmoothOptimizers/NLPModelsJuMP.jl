7fa9795bdd636bbaf4e7fca0d5c69ddccd0dd5d2
Alexis Montoison <alexis.montoison@polymtl.ca>
2020-03-24 22:15:24 -0400

    Update tests and tutorial

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,14 +5,14 @@ using Test, Printf
 nlpmodels_path = joinpath(dirname(pathof(NLPModels)), "..", "test")
 nlpmodels_problems_path = joinpath(nlpmodels_path, "problems")
 
-for problem in [:brownden, :hs5, :hs6, :hs10, :hs11, :hs14]
+for problem in [:brownden, :hs5, :hs6, :hs10, :hs11, :hs14, :lincon]
   include(joinpath("nlp_problems", "$problem.jl"))
   if isfile(joinpath(nlpmodels_problems_path, "$problem.jl"))
     include(joinpath(nlpmodels_problems_path, "$problem.jl"))
   end
 end
 
-for problem in [:lls, :mgh01, :nlshs20, :hs30, :hs43, :mgh07]
+for problem in [:lls, :mgh01, :nlshs20, :hs30, :hs43, :mgh07, :nlslc]
   include(joinpath("nls_problems", "$problem.jl"))
   if isfile(joinpath(nlpmodels_path, "nls_problems", "$problem.jl"))
     include(joinpath(nlpmodels_path, "nls_problems", "$problem.jl"))
