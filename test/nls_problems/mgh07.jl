60c593a128db516c5f70288e8af00d86d03f6dcf
Alexis Montoison <alexis.montoison@polymtl.ca>
2020-06-22 14:18:03 -0400

    Fix tests for NLPModels ≥ 0.12.4

--- test/nls_problems/mgh07.jl
+++ test/nls_problems/mgh07.jl
@@ -8,9 +8,9 @@ function mgh07()
   θ_aux(t) = (t > 0 ? 0.0 : 0.5)
   JuMP.register(nls, :θ_aux, 1, θ_aux, autodiff=true)
 
-  @NLexpression(nls, F1, 10*(x[3] - 10*(atan(x[2]/x[1])/(2*π) + θ_aux(x[1]))))
-  @NLexpression(nls, F2, 10*(sqrt(x[1]^2 + x[2]^2) - 1.0))
-  @NLexpression(nls, F3, 1*x[3])
-  
+  @NLexpression(nls, F1, 1*x[3])
+  @NLexpression(nls, F2, 10*(x[3] - 10*(atan(x[2]/x[1])/(2*π) + θ_aux(x[1]))))
+  @NLexpression(nls, F3, 10*(sqrt(x[1]^2 + x[2]^2) - 1.0))
+
   return MathOptNLSModel(nls, [F1, F2, F3], name="mgh07")
 end
