709c8606a0e97b6551f34d53d058dc29fef022d0
Abel Soares Siqueira <abel.s.siqueira@gmail.com>
2021-07-10 10:43:32 -0300

    :robot: Fix docs to use main

--- docs/make.jl
+++ docs/make.jl
@@ -13,4 +13,4 @@ makedocs(
   pages = Any["Home" => "index.md", "Tutorial" => "tutorial.md", "Reference" => "reference.md"],
 )
 
-deploydocs(repo = "github.com/JuliaSmoothOptimizers/NLPModelsJuMP.jl.git", push_preview = true)
+deploydocs(repo = "github.com/JuliaSmoothOptimizers/NLPModelsJuMP.jl.git", push_preview = true, devbranch = "main")
