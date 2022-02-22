e2db499632409bb7b2f0debba773c7f60d69d1ce
tmigot <tangi.migot@gmail.com>
2022-02-22 09:32:16 -0500

    add hs13

---
+++ test/nlp_problems/hs13.jl
@@ -0,0 +1,11 @@
+"HS13 model"
+function hs13(args...; kwargs...)
+  nlp = Model()
+  @variable(nlp, x[i = 1:2] ≥ 0, start = -2)
+
+  @NLobjective(nlp, Min, (x[1] - 2)^2 + x[2]^2)
+
+  @NLconstraint(nlp, (1 - x[1])^3 ≥ x[2])
+
+  return nlp
+end
