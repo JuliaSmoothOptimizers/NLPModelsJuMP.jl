468e28010ab578fc2aef3b1d4abff036bd7441ca
Abel Soares Siqueira <abel.s.siqueira@gmail.com>
2022-06-30 22:00:36 +0200

    [WIP] Prepare for JuMP 1.2.0

--- test/nls_problems/hs30.jl
+++ test/nls_problems/hs30.jl
@@ -2,9 +2,9 @@
 function hs30()
   model = Model()
   lvar = [1.0; -10.0; -10.0]
-  @variable(model, lvar[i] ≤ x[i = 1:3] ≤ 10, start = 1.0)
+  @variable(model, lvar[i] <= x[i = 1:3] <= 10, start = 1.0)
   @expression(model, F[i = 1:3], x[i])
-  @NLconstraint(model, x[1]^2 + x[2]^2 ≥ 1.0)
+  @NLconstraint(model, x[1]^2 + x[2]^2 >= 1.0)
 
   return MathOptNLSModel(model, F, name = "hs30")
 end
