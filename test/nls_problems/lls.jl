571cdbf183850e051d82936bf533b84c7e9b237e
tmigot <tangi.migot@gmail.com>
2022-08-19 09:01:04 -0400

    fix LLS test problem

--- test/nls_problems/lls.jl
+++ test/nls_problems/lls.jl
@@ -4,7 +4,7 @@ function lls()
 
   @variable(model, x[i = 1:2], start = 0.0)
 
-  @NLconstraint(model, x[1] + x[2] ≥ 0)
+  @constraint(model, x[1] + x[2] ≥ 0)
 
   @expression(model, F1, x[1] - x[2])
   @expression(model, F2, x[1] + x[2] - 2)
