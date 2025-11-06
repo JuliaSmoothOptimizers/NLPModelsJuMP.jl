ee5a4f4b4ddd6f558e293b87e68d934dc63c6aa0
Arnav Kapoor <kapoorarnav43@gmail.com>
2025-11-06 10:25:44 +0530

    Delete test/lencheck.jl

--- test/lencheck.jl
+++
@@ -1,9 +0,0 @@
-using Test
-using NLPModelsJuMP
-
-@testset "lencheck macro" begin
-  x = [1, 2, 3]
-  NLPModels.@lencheck 3 x
-  y = [1, 2]
-  @test_throws ArgumentError NLPModels.@lencheck 3 y
-end
